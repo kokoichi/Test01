e62d1a77d90d9a1c8f6d74cb2e2091f60408bf29
kokoichi <ryujisuganuma0329@yahoo.co.jp>
2016-04-04 18:15:38 +0900

    second commit

---
+++ Test01/Phoneword/Shared/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Shared
+{
+    public class DataService
+    {
+        public static async Task<dynamic> getDataFromService(string queryString)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(queryString);
+
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+            var stream = response.GetResponseStream();
+
+            var streamReader = new StreamReader(stream);
+            string responseText = streamReader.ReadToEnd();
+
+            dynamic data = JsonConvert.DeserializeObject(responseText);
+
+            return data;
+        }
+    }
+}
